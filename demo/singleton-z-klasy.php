0ee7de20f26bb2b1d2465b026205981f6e27e3ac
Tomasz Kuter <evolic@interia.pl>
2017-10-04 07:18:23 +0200

    [WIP][dev] Unloacked checking for constructing instance of Singleton class object and it's clone operation  @todo-request JetBrains IDE does not recognize private __clone() but private __construct() work as it should

--- demo/singleton-z-klasy.php
+++ demo/singleton-z-klasy.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'SingletonInterface.php';
-require_once 'AbstractSingleton.php';
-require_once 'Singleton.php';
+require_once '../src/LoculusEvolution/DesignPatterns/Singleton/SingletonInterface.php';
+require_once '../src/LoculusEvolution/DesignPatterns/Singleton/AbstractSingleton.php';
+require_once '../src/LoculusEvolution/DesignPatterns/Singleton/Singleton.php';
 
 use LoculusEvolution\DesignPatterns\Singleton\Singleton;
 
@@ -11,11 +11,11 @@ $objectOne = Singleton::getInstance();
 
 echo 'test-2.', PHP_EOL;
 echo '  skipping..', PHP_EOL;
-//$object2 = new Singleton();
+$object2 = new Singleton();
 
 echo 'test-3.', PHP_EOL;
 echo '  skipping..', PHP_EOL;
-//$object3 = clone $objectOne;
+$object3 = clone $objectOne;
 
 echo 'done.', PHP_EOL;
 
